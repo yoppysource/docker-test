e10ece2c41b66b91e93614544f821b8a1aee792e
Yoppy <busyru98@gmail.com>
2022-06-13 20:37:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react sd
+          Learn React
         </a>
       </header>
     </div>
